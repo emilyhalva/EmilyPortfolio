5494af1e611897820e137ca7843d5ef012d1430f
Emily Halva <ehalva@asu.edu>
2024-02-11 23:07:32 -0700

    Added card components for collection

--- emily-portfolio/src/components/Collection.vue
+++ emily-portfolio/src/components/Collection.vue
@@ -1,41 +1,271 @@
 <script>
-
+export default {
+  name: "Collection",
+  data() {
+    return {
+      clothes: [
+        {
+          name: "Cat Beanie",
+          descriptions: "cat description",
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Strawberry Cat Beanie",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Regular Beanie",
+          descriptions: 40,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Sunset Shorts",
+          descriptions: 40,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Grey Shorts",
+          descriptions: 50,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Hexagon Cardigan",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Grunge Mesh Sweater",
+          descriptions: 50,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Long Sleeve Top",
+          descriptions: 50,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+      ],
+      flowers: [
+        {
+          name: "Lavander",
+          descriptions: "pruple  description",
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "White Roses",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Tulips",
+          descriptions: 40,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "White Roses",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Kylie",
+          descriptions: 40,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+      ],
+      stuffedAnimals: [
+        {
+          name: "Chunky Bee",
+          descriptions: "pruple  description",
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Small Bee",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Gnome",
+          descriptions: 40,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+      ],
+      others: [
+        {
+          name: "Chunky Blanket",
+          descriptions: "pruple  description",
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Peach Coaster",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Orange Coaster",
+          descriptions: 40,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Mug Cozy",
+          descriptions: 30,
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          name: "Farmers Market Tote",
+          descriptions: "pruple  description",
+          link: "https://via.placeholder.com/150",
+          image: "https://via.placeholder.com/150",
+        },
+      ],
+    };
+  },
+  methods: {
+    sampleMethod() {},
+  },
+};
 </script>
 
 <template>
-    <div id="collection"></div>
-    <div class="Collection">
-        <h1>Collection</h1>
+  <div id="collection"></div>
+  <div class="Collection">
+    <v-container>
+      <h1>Collection</h1>
 
-        <div id="clothingSegment">
+      <div id="clothingSegment">
+        <h2>Clothing</h2>
+        <!-- Row for cards -->
+        <v-row>
+          <v-col
+            v-for="clothingPiece in clothes"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card>
+              <v-img
+                src="https://via.placeholder.com/150"
+                height="200px"
+              ></v-img>
+              <v-card-title>{{ clothingPiece.name }}</v-card-title>
+              <v-card-subtitle>{{
+                clothingPiece.descriptions
+              }}</v-card-subtitle>
+              <v-card-actions>
+                <v-btn text>Tutorial</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
 
-        </div>
+      <div id="flowersSegment">
+        <h2>Flowers</h2>
+        <!-- Row for cards -->
+        <v-row>
+          <v-col v-for="flowerPiece in flowers" cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img
+                src="https://via.placeholder.com/150"
+                height="200px"
+              ></v-img>
+              <v-card-title>{{ flowerPiece.name }}</v-card-title>
+              <v-card-subtitle>{{ flowerPiece.descriptions }}</v-card-subtitle>
+              <v-card-actions>
+                <v-btn text>Tutorial</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
 
-        <div id="flowersSegment">
+      <div id="stuffedAnimalsSegment">
+        <h2>Cuddle Buddies and Cutie Patooties</h2>
+        <!-- Row for cards -->
+        <v-row>
+          <v-col
+            v-for="stuffedAnimal in stuffedAnimals"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card>
+              <v-img
+                src="https://via.placeholder.com/150"
+                height="200px"
+              ></v-img>
+              <v-card-title>{{ stuffedAnimal.name }}</v-card-title>
+              <v-card-subtitle>{{
+                stuffedAnimal.descriptions
+              }}</v-card-subtitle>
+              <v-card-actions>
+                <v-btn text>Tutorial</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
 
-        </div>
-
-        <div id="othersSegment">
-
-        </div>
-    </div>
+      <div id="othersSegment">
+        <h2>Miscelaneous Projects</h2>
+        <!-- Row for cards -->
+        <v-row>
+          <v-col v-for="otherPiece in others" cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img
+                src="https://via.placeholder.com/150"
+                height="200px"
+              ></v-img>
+              <v-card-title>{{ otherPiece.name }}</v-card-title>
+              <v-card-subtitle>{{ otherPiece.descriptions }}</v-card-subtitle>
+              <v-card-actions>
+                <v-btn text>Tutorial</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <style>
-    .Collection {
-        background-color: #f9f7f3;
-    }
+.Collection {
+  background-color: #f9f7f3;
+}
 
-    #clothingSegment {
-        background-color: #f9f7f3;
-        height: 500px;
-    }
-    #flowersSegment {
-        background-color: #DEEBE9;
-        height: 500px;
-    }
-    #othersSegment {
-        background-color: #FFF2F7;
-        height: 500px;
-    }
+#clothingSegment {
+  background-color: #f7ede2;
+}
+#flowersSegment {
+  background-color: #deebe9;
+}
+#stuffedAnimalsSegment {
+  background-color: #fef8f2;
+}
+
+#othersSegment {
+  background-color: #fff2f7;
+}
 </style>
--- emily-portfolio/src/components/Home.vue
+++ emily-portfolio/src/components/Home.vue
@@ -1,40 +1,41 @@
 <template>
-    <div id="home"></div>
-    <div class="Home">
-        <div class="content">
-            <div id="welcomeMsg">   
-                <h2> Unwind and Explore: My Cozy Crochet Corner</h2>
-                <p>I'm happy you're here :)</p>
-            </div>
-            <img id="collageImg" src="../Resources/crochetCollage.png">
-        </div>
+  <div id="home"></div>
+  <div class="Home">
+    <div class="content">
+      <div id="welcomeMsg">
+        <h2>Unwind and Explore: My Cozy Crochet Corner</h2>
+        <p>Take a look around :)</p>
+      </div>
+      <img id="collageImg" src="../Resources/crochetCollage.png" />
     </div>
+  </div>
 </template>
 
 <style>
-    .Home {
-        height: 100vh;
-        background-image: url("../Resources/GradientOverlay.png");
-        background-size: cover;  /* Makes background img responsive */
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .content {
-        display: flex;
-        align-items: center;
-    }
-    #welcomeMsg {
-        text-align: left;
-        margin-left: 60px;
-        padding-right: 100px;
-        font-size: 2em;
-        color: #156669;
-        font-family: Oswald;
-    }
-    #collageImg {
-        width: 450px;
-        margin-right: 60px;
-        border-radius: 100%;
-    }
+.Home {
+  height: 100vh;
+  background-image: url("../Resources/GradientOverlay.png");
+  background-size: cover; /* Makes background img responsive */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content {
+  display: flex;
+  align-items: center;
+}
+#welcomeMsg {
+  text-align: left;
+  margin-left: 60px;
+  padding-right: 100px;
+  font-size: 2em;
+  color: #156669;
+  font-family: Oswald;
+  flex: wrap;
+}
+#collageImg {
+  width: 500px;
+  margin-right: 60px;
+  border-radius: 100%;
+}
 </style>
