87938af95af3993d9645660760e95d794b9da08c
Emily Halva <ekhalva@gmail.com>
2024-03-20 20:32:02 -0700

    Deployment on heroku

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('emily-portfolio/dist'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'emily-portfolio', 'dist', 'index.html'));
-    });
-}
-
-app.listen(port, () => {
-  console.log('Server is listening to port ' + port);
-});
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const app = express()
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+const port = process.env.PORT || 8080
+app.listen(port)
